5e6c7aa745d5f731859cd46434f0786267d0d86e
Duy Phann <phanthanhduy.jin@gmail.com>
2024-02-28 14:35:27 +0700

    update: send data to auto login with Brekeke invoke

--- src/Control.tsx
+++ src/Control.tsx
@@ -16,8 +16,14 @@ export const Control = () => {
   const [visible, setVisible] = useState(false)
 
   const linkToBrekeke = async () => {
-    const params = qs.stringify({ user: 'Duy', password: '111111' })
-    console.log('#Duy Phan console', params)
+    const params = qs.stringify({
+      host: 'dev01.brekeke.com',
+      port: 8443,
+      tenant: 'nam',
+      user: 'nam01',
+      password: 123,
+      action: 'invoke-example'
+    })
     try {
       setVisible(false)
       await Linking.openURL(`brekekeapp_phonedev://open?${params}`)
@@ -61,7 +67,7 @@ export const Control = () => {
                     path='M9,11.75A1.25,1.25 0 0,0 7.75,13A1.25,1.25 0 0,0 9,14.25A1.25,1.25 0 0,0 10.25,13A1.25,1.25 0 0,0 9,11.75M15,11.75A1.25,1.25 0 0,0 13.75,13A1.25,1.25 0 0,0 15,14.25A1.25,1.25 0 0,0 16.25,13A1.25,1.25 0 0,0 15,11.75M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20C7.59,20 4,16.41 4,12C4,11.71 4,11.42 4.05,11.14C6.41,10.09 8.28,8.16 9.26,5.77C11.07,8.33 14.05,10 17.42,10C18.2,10 18.95,9.91 19.67,9.74C19.88,10.45 20,11.21 20,12C20,16.41 16.41,20 12,20Z'
                   />
                 </View>
-                <Text>Hotel reception</Text>
+                <Text>Make call to nam05</Text>
               </TouchableOpacity>
               <TouchableOpacity
                 style={styles.optionBox}
@@ -73,7 +79,7 @@ export const Control = () => {
                     path='M12,19A2,2 0 0,0 10,21A2,2 0 0,0 12,23A2,2 0 0,0 14,21A2,2 0 0,0 12,19M6,1A2,2 0 0,0 4,3A2,2 0 0,0 6,5A2,2 0 0,0 8,3A2,2 0 0,0 6,1M6,7A2,2 0 0,0 4,9A2,2 0 0,0 6,11A2,2 0 0,0 8,9A2,2 0 0,0 6,7M6,13A2,2 0 0,0 4,15A2,2 0 0,0 6,17A2,2 0 0,0 8,15A2,2 0 0,0 6,13M18,5A2,2 0 0,0 20,3A2,2 0 0,0 18,1A2,2 0 0,0 16,3A2,2 0 0,0 18,5M12,13A2,2 0 0,0 10,15A2,2 0 0,0 12,17A2,2 0 0,0 14,15A2,2 0 0,0 12,13M18,13A2,2 0 0,0 16,15A2,2 0 0,0 18,17A2,2 0 0,0 20,15A2,2 0 0,0 18,13M18,7A2,2 0 0,0 16,9A2,2 0 0,0 18,11A2,2 0 0,0 20,9A2,2 0 0,0 18,7M12,7A2,2 0 0,0 10,9A2,2 0 0,0 12,11A2,2 0 0,0 14,9A2,2 0 0,0 12,7M12,1A2,2 0 0,0 10,3A2,2 0 0,0 12,5A2,2 0 0,0 14,3A2,2 0 0,0 12,1Z'
                   />
                 </View>
-                <Text>Another</Text>
+                <Text>Open keypad</Text>
               </TouchableOpacity>
             </View>
           </View>
